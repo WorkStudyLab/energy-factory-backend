8da1fd4a0b3a489c6172b12d8f97dc1ae02cf69d
seongheonjeong <js504040@naver.com>
2025-09-06 21:49:33 +0900

    Initial commit

---
+++ src/main/java/com/energyfactory/energy_factory/EnergyFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.energyfactory.energy_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energyfactory/energy_factory/EnergyFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energyfactory.energy_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
