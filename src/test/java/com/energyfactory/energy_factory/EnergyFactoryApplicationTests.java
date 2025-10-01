315f5eafb7b728c95975793b97745c1b94317e18
seongheonjeong <js504040@naver.com>
2025-10-01 22:56:45 +0900

    modify : 테스트코드 주석

--- src/test/java/com/energyfactory/energy_factory/EnergyFactoryApplicationTests.java
+++ src/test/java/com/energyfactory/energy_factory/EnergyFactoryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.energyfactory.energy_factory;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EnergyFactoryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.energyfactory.energy_factory;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EnergyFactoryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
